ad4d6dc52635da92059fee44f92bac1245c4b4ed
akinfemi <akinfemi.aluko@gmail.com>
2017-11-16 19:40:19 -0800

    basic

--- client_src/client.c
+++ client_src/client.c
@@ -5,9 +5,42 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: akinfemi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/16 11:39:34 by akinfemi          #+#    #+#             */
-/*   Updated: 2017/11/16 11:39:36 by akinfemi         ###   ########.fr       */
+/*   Created: 2017/11/16 11:39:16 by akinfemi          #+#    #+#             */
+/*   Updated: 2017/11/16 19:39:01 by akinfemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/ftp.h"
 
+int		create_client(char *ip_addr, int port)
+{
+	int			sockt;
+	struct protoent		*protocol;
+	struct sockaddr_in	addr;
+
+	protocol = getprotobyname("tcp");
+	if (!protocol)
+		print_error(1);
+	sockt = socket(PF_INET, SOCK_STREAM, protocol->p_proto);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = inet_addr(ip_addr);
+	if(connect(sockt, (const struct sockaddr *)&addr, sizeof(addr)) == -1)
+		print_error(2);
+	listen(sockt, 42);
+	return(sockt);
+}
+
+int		main(int ac, char **av)
+{
+	int			port;
+	int			socket;
+
+	if (ac != 3)
+		usage(av[0]);
+	port = ft_atoi(av[2]);
+	socket = create_client(av[1], port);
+	write(socket, "hello\n", 6);
+	close(socket);
+	return(0);
+}
--- client_src/error.c
+++ client_src/error.c
@@ -6,7 +6,7 @@
 /*   By: akinfemi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 12:22:26 by akinfemi          #+#    #+#             */
-/*   Updated: 2017/11/16 14:11:40 by akinfemi         ###   ########.fr       */
+/*   Updated: 2017/11/16 19:33:10 by akinfemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@ void		print_error(int error)
 	{
 		ft_printf("No protocol number in the /etc/protocol file\n");
 		exit(-1);
+	}
+	else if (error == 2)
+	{
+		ft_printf("Connection error.\n");
+		exit(2);
 	}
 }
